7c432d66075661034943233825f300037b70291b
wellyekacipta <wellyekacipta.wec@gmail.com>
2026-09-22 09:17:15 +0700

    Fix Section namespace import in DocumentationCategoryForm to Filament\Schemas\Components\Section

--- app/Filament/Resources/Documentations/Schemas/DocumentationCategoryForm.php
+++ app/Filament/Resources/Documentations/Schemas/DocumentationCategoryForm.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources\Documentations\Schemas;
 
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Repeater;
-use Filament\Forms\Components\Section;
+use Filament\Schemas\Components\Section;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
